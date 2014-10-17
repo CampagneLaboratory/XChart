f68a65fc15a263c5c2b571cd2be220f9c0087550
Manuele Simi <manuele.simi@campagnelab.org>
2014-10-17 17:00:43 -0400

    Do not need to rename the artifact before the deployment.

--- deploy-RELEASE-artifacts.sh
+++ deploy-RELEASE-artifacts.sh
@@ -5,17 +5,10 @@
 
 ARTIFACT_FOLDER=build/artifacts/XChart/
 
-original_filename=$(basename ${ARTIFACT_FOLDER}/XChart_*.zip)
-filename="${original_filename%.*}"
+filename=$(basename ${ARTIFACT_FOLDER}/XChart_*.zip)
+filename="${filename%.*}"
 version=$(echo ${filename} | cut -d"_" -f2)
 echo "detected version: ${version}"
-
-#if build number is defined, we use it as minor version
-if [ -n "$BUILD_NUMBER" ]; then
-	version=$(echo ${version} | cut -d"." -f1,2).${BUILD_NUMBER}
-	mv ${ARTIFACT_FOLDER}/${original_filename} ${ARTIFACT_FOLDER}/XChart_${version}.zip
-	echo "version with build: ${version}"
-fi
 
 $1mvn deploy:deploy-file -DgroupId=org.campagnelab.mps \
   -DartifactId=xchart \
